3c5f609971e72f1b1062ee2d0b8d9802160dbdab
Kartik <kartik2498@outlook.com>
2017-10-03 13:26:43 +0530

    color made opaque

--- app/src/main/java/com/example/kartik/movies/Parser.java
+++ app/src/main/java/com/example/kartik/movies/Parser.java
@@ -4,10 +4,6 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.util.LongSparseArray;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
 import android.widget.Toast;
 
 import com.example.kartik.movies.database.MovieContentProvider;
